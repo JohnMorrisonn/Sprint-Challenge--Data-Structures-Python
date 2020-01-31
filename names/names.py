c6163497d780e79e5bed93b9dce5dfd727f5e346
John Morrison <jjosephmorrison@gmail.com>
2020-01-31 13:24:42 -0500

    SC

--- names/names.py
+++ names/names.py
@@ -1,5 +1,102 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if (value < self.value):
+            if (self.left is None):
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        elif (value >= self.value):
+            if (self.right is None):
+                self.right = BinarySearchTree(value)
+
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        elif target > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if self.right is None:
+            maximum = self.value
+            return maximum
+        else:
+            return self.right.get_max()
+        
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left is not None and self.right is not None:
+            self.left.for_each(cb)
+            self.right.for_each(cb)
+        elif self.left is not None:
+            self.left.for_each(cb)
+        elif self.right is not None:
+            self.right.for_each(cb)
+        else:
+            return
+
+
+    # DAY 2 Project -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_print(self, node):
+        if self.left:
+            self.left.in_order_print(node.left)
+        print(self.value)
+        if self.right:
+            self.right.in_order_print(node.right)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        q = Queue()
+        q.enqueue(node)
+        while q.len() > 0:
+            temp = q.dequeue()
+            print(temp)
+            if temp.left:
+                q.enqueue(temp.left)
+            if temp.right:
+                q.enqueue(temp.right)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        stack = Stack()
+        stack.push(node)
+        while stack.len() > 0:
+            temp = stack.pop()
+            print(temp)
+            if temp.left:
+                stack.push(temp.left)
+            if temp.right:
+                stack.push(temp.right)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,11 +107,20 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Create BST
+names_1_bst = BinarySearchTree('a')
+
 duplicates = []
+
+# Add names to each BST
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    names_1_bst.insert(name_1)
+
+
+# Add names that bst2 contains from bst1 and append to duplicates
+for name_2 in names_2:
+    if names_1_bst.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,15 +6,41 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        self.length = 0
 
     def append(self, item):
-        pass
+        # Increment length
+
+        # If full, overwrite oldest value and add new value to head
+        if self.length == self.capacity:
+
+            if self.current.next is not None:
+                self.current.value = item
+                self.current = self.current.next
+
+            elif self.current.next is None:
+                self.storage.tail.value = item
+                self.current = self.storage.head
+
+            
+        # If not full, add value in 
+        else:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+            self.length += 1
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+        if self.length == 0:
+            return []
 
-        # TODO: Your code here
+        else:
+            current = self.storage.head
+            while current:
+                list_buffer_contents.append(current.value)
+                current = current.next
+        # Append current.next
 
         return list_buffer_contents
 
